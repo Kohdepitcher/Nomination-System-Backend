6b26d2d7a4151e6b92a3c2f036105aa64639ea1e
Kohde Pitcher <kohde1@hotmail.com>
2021-01-13 01:50:32 +1000

    refactored routes to use new controller funcs

---
+++ functions/src/routes/users-routes-config.ts
@@ -0,0 +1,59 @@
+import { Application } from "express";
+
+import { isAuthenticated } from "../auth/authenticated";
+import { isAuthorized } from "../auth/authorized";
+
+//import the user controller
+import { userController } from '../users/User-Controller'
+// import { AuthRoles } from "../globals";
+
+export function routesConfig(app: Application) {
+    
+    // creates user
+    app.post('/users', [
+       isAuthenticated,
+       isAuthorized({ hasRole: ['admin', 'manager'] }),
+       new userController().createUser
+    ]);
+
+     //DONT USE - FOR TESTING ONLY
+     app.post('/user-in-db/', [
+        isAuthenticated,
+        isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+        new userController().createUserInDB
+    ]);
+
+    // lists all users
+    app.get('/users', [
+        isAuthenticated,
+        isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+        new userController().getAllUsers
+    ]);
+
+    // get :id user
+    app.get('/users/:uid', [
+        isAuthenticated,
+        isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+        new userController().getUser
+    ]);
+
+    // updates :id user
+    app.patch('/users/:uid', [
+        isAuthenticated,
+        isAuthorized({ hasRole: ['admin', 'manager'], allowSameUser: true }),
+        new userController().patchUser
+    ]);
+
+    // app.patch('/users/self_patch/', [
+    //     isAuthenticated,
+    //     isAuthorized({ hasRole: [AuthRoles.Admin, AuthRoles.Trainer], allowSameUser: true}),
+    //     new userController().updateUser
+    // ])
+
+    // deletes :id user
+    app.delete('/users/:uid', [
+        isAuthenticated,
+        isAuthorized({ hasRole: ['admin', 'manager'] }),
+        new userController().removeUser
+    ]);
+ }
