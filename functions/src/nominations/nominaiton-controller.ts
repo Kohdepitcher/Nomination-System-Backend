ab26220dbd1f3d2bfee87858ca170d40096d54ed
Kohde Pitcher <kohde1@hotmail.com>
2021-02-26 01:31:41 +1000

    added create nomination on behalf function

--- functions/src/nominations/nominaiton-controller.ts
+++ functions/src/nominations/nominaiton-controller.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 
 import { connect } from '../config';
 
+// import { AuthRoles } from '../globals';
+
 //trial date entity
 //import { TrialMeeting } from "../entities/trialDate";
 import { Nomination } from '../entities/nomination'
@@ -13,7 +15,9 @@ import { User } from '../entities/user'
 //import { Equal} from "typeorm";
 
 export class NominationController {
+
     
+
     //CREATE
     /*
     This method is designed to create Nominations for a specific Trial Meeting
@@ -109,6 +113,109 @@ export class NominationController {
         }
         
     }
+
+    /*
+    This method is designed to create Nominations for a specific Trial Meeting
+
+    
+    
+    NOTE: This method requires the following in the body
+    Horse Name - Name of the horse
+    Horse Age - How old the horse is
+    Horse Class - The class of the horse
+    Jockey - Name of the jockey ridding the horse
+    Meeting ID - the ID for the meeting that is being nominated for
+    
+    This method will not deny any nominations after the closing time
+    as its intended to be used by admins
+    */
+   async createNominationForMeetingOnBehalfOfTrainer(request: Request, response: Response) {
+        
+    //body elements
+    const { jockey, horseName, horseAge, horseClass, trialMeetingID, trainerID } = request.body;
+    
+    //Throw 400 response errors for required missing body parts
+    if (!horseName) {
+        return response.status(400).send({ message: 'Missing name for the horse' })
+    }
+    
+    if (!horseAge) {
+        return response.status(400).send({ message: 'Missing age for the horse: ' + horseName })
+    }
+    
+    if (!horseClass) {
+        return response.status(400).send({ message: 'Missing class for the horse: ' + horseName })
+    }
+    
+    if (!trialMeetingID) {
+        return response.status(400).send({ message: 'Missing the ID of the Jumpouts that are being nominated for' })
+    }
+
+    if (!trainerID) {
+        return response.status(400).send({ message: 'Missing the ID of the trainer that are being nominated on behalf of' })
+    }
+    
+    
+    try {
+        
+        //create DB Connection
+        const connection = await connect();
+        
+        //Repos
+        const meetingRepo = connection.getRepository(TrialMeeting);
+        const userRepo = connection.getRepository(User)
+        const nominationRepo = connection.getRepository(Nomination);
+        
+        //Get the matching jumpout meeting
+        const matchingMeeting = await meetingRepo.findOne(trialMeetingID);
+        
+        //if the matching meeting is null, respond with an 400 error
+        if(!matchingMeeting) {
+            return response.status(400).send({ message: 'No jumpouts where found for ID: ' + trialMeetingID });
+        }
+        
+        // //check if the UTC time is past the UTC cut off time for the selected jumpouts
+        // //if so send back an error message
+        // if (new Date() > matchingMeeting.closeDate) {
+        //     return response.status(400).send({ message: 'Sorry, nominations have closed for these Jumpouts' })
+        // }
+        
+        //get the trainer for nomination
+        const matchingUser = await userRepo.findOne({ where: { userID: trainerID } })
+        
+        //if the matching user is null, respond with an 400 error
+        if(!matchingUser) {
+            return response.status(400).send({ message: 'No user was found for ID: ' + response.locals.uid });
+        }
+        
+        
+        
+        //create new nomination
+        var newNomination = new Nomination();
+        
+        //set the properties
+        newNomination.horseName = horseName;
+        newNomination.horseAge = horseAge;
+        newNomination.horseClass = horseClass;
+        newNomination.jockey = jockey;
+        
+        //set the jumpouts for the nomination
+        newNomination.trialDate = matchingMeeting;
+        
+        //set the trainer for the nomination
+        newNomination.user = matchingUser;
+        
+        const savedNomination = nominationRepo.save(newNomination);
+        
+        return response.status(200).send(savedNomination);
+        
+    } catch (err) {
+        return handleError(response, err);
+    }
+    
+}
+
+
 
     //READ
     /*
@@ -283,7 +390,6 @@ export class NominationController {
             .execute();
 
             return response.status(200).send(updatedNomination)
-
         } catch (error) {
             return handleError(response, error)
         }
@@ -391,76 +497,76 @@ export async function updateNomination (request: Request, response: Response) {
 };
 
 //get all nominations
-export async function getAllNominations (request: Request, response: Response) {
+// export async function getAllNominations (request: Request, response: Response) {
     
-    //query parameters
-    const {trialID} = request.query;
+//     //query parameters
+//     const {trialID} = request.query;
     
     
-    const connection = await connect();
-    const nominationRepo = connection.getRepository(Nomination);
+//     const connection = await connect();
+//     const nominationRepo = connection.getRepository(Nomination);
     
-    // Get all rows
-    var allNominations: Nomination[]
+//     // Get all rows
+//     var allNominations: Nomination[]
     
-    if (trialID != null) {
-        allNominations = await nominationRepo
-        .createQueryBuilder('nomination')
-        .leftJoinAndSelect('nomination.trialDate', 'TrialMeeting')
-        .leftJoinAndSelect('nomination.user', 'User')
-        .where('nomination.trialDate.meetingId = :id', { id: trialID})
-        .getMany();
-        // allNominations = await nominationRepo.find({
+//     if (trialID != null) {
+//         allNominations = await nominationRepo
+//         .createQueryBuilder('nomination')
+//         .leftJoinAndSelect('nomination.trialDate', 'TrialMeeting')
+//         .leftJoinAndSelect('nomination.user', 'User')
+//         .where('nomination.trialDate.meetingId = :id', { id: trialID})
+//         .getMany();
+//         // allNominations = await nominationRepo.find({
         
-        //     //only where the dates are greater than passed date
-        //     where: {
-        //         //date: MoreThan(new Date(Date.parse(afterDate)))
-        //         trialDate: Equal(trialID)
-        //     },
+//         //     //only where the dates are greater than passed date
+//         //     where: {
+//         //         //date: MoreThan(new Date(Date.parse(afterDate)))
+//         //         trialDate: Equal(trialID)
+//         //     },
         
-        //     //oder by the date
-        //     // order: {
-        //     //     //date: 'ASC'
-        //     // },
+//         //     //oder by the date
+//         //     // order: {
+//         //     //     //date: 'ASC'
+//         //     // },
         
-        //     //skip none
-        //     //skip: 0,
+//         //     //skip none
+//         //     //skip: 0,
         
-        //     //first 5 as decided by order vy
-        //     //take: 5
+//         //     //first 5 as decided by order vy
+//         //     //take: 5
         
-        // })
-    } else {
-        allNominations = await nominationRepo
-        .createQueryBuilder('nomination')
-        .leftJoinAndSelect('nomination.trialDate', 'TrialMeeting')
-        .leftJoinAndSelect('nomination.user', 'User')
-        .getMany();
-    }
+//         // })
+//     } else {
+//         allNominations = await nominationRepo
+//         .createQueryBuilder('nomination')
+//         .leftJoinAndSelect('nomination.trialDate', 'TrialMeeting')
+//         .leftJoinAndSelect('nomination.user', 'User')
+//         .getMany();
+//     }
     
     
     
-    response.status(200).send(allNominations);
+//     response.status(200).send(allNominations);
     
-};
+// };
 
 //get all nominations
-export async function getAllNominationsWithTrialDates (request: Request, response: Response) {
+// export async function getAllNominationsWithTrialDates (request: Request, response: Response) {
     
     
-    const connection = await connect();
-    const nominationRepo = connection.getRepository(Nomination);
+//     const connection = await connect();
+//     const nominationRepo = connection.getRepository(Nomination);
     
-    // Get all rows
-    const allTrialMeetings = await nominationRepo
-    .createQueryBuilder('nomination')
-    .leftJoinAndSelect('nomination.trialDate', 'TrialMeeting')
-    .leftJoinAndSelect('nomination.user', 'User')
-    .getMany();
+//     // Get all rows
+//     const allTrialMeetings = await nominationRepo
+//     .createQueryBuilder('nomination')
+//     .leftJoinAndSelect('nomination.trialDate', 'TrialMeeting')
+//     .leftJoinAndSelect('nomination.user', 'User')
+//     .getMany();
     
-    response.status(200).send(allTrialMeetings);
+//     response.status(200).send(allTrialMeetings);
     
-};
+// };
 
 //get specific nomination
 export async function getSpecificNomination (request: Request, response: Response) {
@@ -490,50 +596,50 @@ export async function getSpecificNomination (request: Request, response: Respons
     
 };
 
-export async function getTrainersAndNominationsCountForMeeting(request: Request, response: Response) {
+// export async function getTrainersAndNominationsCountForMeeting(request: Request, response: Response) {
     
-    const {meetingID} = request.params;
+//     const {meetingID} = request.params;
     
-    try {
+//     try {
         
-        const connection = await connect();
-        const nominationRepo = connection.getRepository(Nomination);
-        // const meetingRepo = connection.getRepository(TrialMeeting);
+//         const connection = await connect();
+//         const nominationRepo = connection.getRepository(Nomination);
+//         // const meetingRepo = connection.getRepository(TrialMeeting);
         
-        let groupedAndCounted = await nominationRepo.createQueryBuilder('nomination')
+//         let groupedAndCounted = await nominationRepo.createQueryBuilder('nomination')
         
-        //join on trial meeting
-        .leftJoin('nomination.trialDate', "trialDate")
+//         //join on trial meeting
+//         .leftJoin('nomination.trialDate', "trialDate")
         
-        //join on user
-        .leftJoinAndSelect('nomination.user', 'User')
+//         //join on user
+//         .leftJoinAndSelect('nomination.user', 'User')
         
-        //select distinct just user name and count of userIDs in nominations as count
-        .select(['DISTINCT(User.name)', 'COUNT(User.userID) AS count'])
+//         //select distinct just user name and count of userIDs in nominations as count
+//         .select(['DISTINCT(User.name)', 'COUNT(User.userID) AS count'])
         
-        //group by name of the user
-        .groupBy('user.name')
+//         //group by name of the user
+//         .groupBy('user.name')
         
-        //only execute for the meeting that matches the meetingID
-        .where('trialDate.meetingID = :id', { id: meetingID })
+//         //only execute for the meeting that matches the meetingID
+//         .where('trialDate.meetingID = :id', { id: meetingID })
         
-        //filter out scratched nominations
-        .andWhere('nomination.isScratched = false')
+//         //filter out scratched nominations
+//         .andWhere('nomination.isScratched = false')
         
-        //return raw result since it doesnt match an entity
-        .getRawMany()
-        
-        
+//         //return raw result since it doesnt match an entity
+//         .getRawMany()
         
         
-        //send the array to the client
-        return response.status(200).send(groupedAndCounted);
         
-    } catch (error) {
-        return handleError(response, error)
-    }
+        
+//         //send the array to the client
+//         return response.status(200).send(groupedAndCounted);
+        
+//     } catch (error) {
+//         return handleError(response, error)
+//     }
     
-}
+// }
 
 //update specific nomination
 export async function deleteNomination (request: Request, response: Response) {
